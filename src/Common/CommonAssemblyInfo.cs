034c2a30b3378743b0ddc4e16d1e5a04476c9338
Brett Samblanet <brettsam@microsoft.com>
2016-02-26 17:41:31 -0800

    updating assemblyinfo

---
+++ src/Common/CommonAssemblyInfo.cs
@@ -0,0 +1,25 @@
+// ----------------------------------------------------------------------------
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// ----------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+// Common assembly information.
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyCopyright("© Microsoft Corporation.  All rights reserved.")]
+[assembly: AssemblyProduct("Microsoft® Azure WebJobs Mobile Extensions")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: AssemblyConfiguration("")]
+[assembly: NeutralResourcesLanguage("en-US")]
+[assembly: AssemblyMetadata("Serviceable", "True")]
+
+// ===========================================================================
+//  Only edit the first two parts of the versions below.
+//  The official build will automatically generate the last two parts.
+// ===========================================================================
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
